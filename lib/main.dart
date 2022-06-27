01f703c992c7e497962bb420acc7fddf90c0f4c5
Adrien <adrienberard@hotmail.fr>
2022-06-27 09:42:32 +0200

    resolu bug web

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,20 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:rlcesi/firebase_options.dart';
 import 'package:rlcesi/pages/Authentication/VerifyEmailScreen.dart';
 import 'package:rlcesi/pages/wrapper/wrapperAuthScreen.dart';
 import 'package:rlcesi/pages/wrapper/wrapperHomeScreen.dart';
 
 Future main() async {
    WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: FirebaseOptions(
+      apiKey: 'AIzaSyASUZDrCZvytb0w6LPAfIvCR8frbsYUdKw',
+      appId: '1:597163300495:web:befb0dc6df7bbb2e9142fa',
+      messagingSenderId: '597163300495',
+      projectId: 'rlcesi-bb20d',)
+  );
   runApp(MyApp());
 }
 final navigatorKey = GlobalKey<NavigatorState>();
